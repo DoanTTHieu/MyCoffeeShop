5d8e47632751e9e913385e142f675fe6d6fd8df3
hieu <18520734@gm.uit.edu.vn>
2020-05-25 08:54:25 +0700

    create OrdersHistory UI

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Auth from './src/components/Auth/Auth';
+//import Auth from './src/components/Auth/Auth';
+import OrdersHistory from './src/components/Menu/OrdersHistory';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Auth/>
+      <OrdersHistory/>
     </NavigationContainer>
   );
 }
